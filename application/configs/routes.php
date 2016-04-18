d2f0a90c97b7d865e401a1d07d64efc41748f33d
hepanming007 <hepanming007@126.com>
2016-04-18 13:34:57 +0800

    Create routes.php

---
+++ application/configs/routes.php
@@ -0,0 +1,5 @@
+<?php
+
+$route['fenxiao_jipiao_index_(\d+)_(\d+)'] = 'fenxiao/jipiao/index?page=$1&perpage=$2';
+$route['fenxiao_jipiao_test_model']        = 'fenxiao/jipiao/test_model';
+return $route;
